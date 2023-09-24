7ec001494de8229a1fb8924c42069a988a8f9445
Alejandra Ospina <aleospinagracia@hotmail.com>
2023-09-24 12:08:30 -0500

    few final changes

--- Proyecto_2/src/main/java/proyecto_2/proyecto_2/Login.java
+++ Proyecto_2/src/main/java/proyecto_2/proyecto_2/Login.java
@@ -1,7 +1,6 @@
 package proyecto_2.proyecto_2;
 
 import javax.enterprise.context.SessionScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 // or import javax.faces.bean.SessionScoped;
@@ -10,18 +9,25 @@ import java.io.Serializable;
 @SessionScoped
 public class Login implements Serializable {
 
-
     private String name;
     private String password;
 
-    @Inject
-    private ChatAppBean chat;
+    public String getName() {
+        return name;
+    }
 
-    public String getName() { return name; }
-    public void setName(String newValue) { name = newValue; }
+    public void setName(String newValue) {
+        name = newValue;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String newValue) { password = newValue; }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String newValue) {
+        password = newValue;
+    }
+
     public String includeUser() {
         return "chat.xhtml";
     }
